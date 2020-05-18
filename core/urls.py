87c3e2b0301d4da012e9d36911cb679ed665d8ef
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-05-18 10:27:48 -0300

    Incluindo statics files e tratativas de erros

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,21 @@
-"""core URL Configuration
+from django.conf.urls import handler400, handler403, handler404, handler500
+from django.urls import path, include
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('django.contrib.auth.urls'), name='login'),
+    path('', include('example.urls')),
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if not settings.DEBUG:
+    handler400 = 'example.views.bad_request'
+    handler403 = 'example.views.permission_denied'
+    handler404 = 'example.views.page_not_found'
+    handler500 = 'example.views.server_error'
